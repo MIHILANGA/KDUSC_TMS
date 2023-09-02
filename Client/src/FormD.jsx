30dfcf4d2e0b349527432bd5a3667016a589a93b
MAVKT Muramudali <136581189+VilharaMuramudali@users.noreply.github.com>
2023-09-03 00:52:45 +0530

    Admin request pannel

--- Client/src/FormD.jsx
+++ Client/src/FormD.jsx
@@ -16,6 +16,7 @@ function FormD({ showNotification }) {
         const filteredData = response.data.data.map(form => ({
           ...form, // Keep all existing data
           rejectOrConfirm: '', // Add a new property for reject/confirm input
+          message: '', // Add a new property for the message
         }));
         showNotification(filteredData);
       })
@@ -37,12 +38,13 @@ function FormD({ showNotification }) {
 
   const handleFormSubmit = (index) => {
     const updatedFormData = [...formData];
-    const { _id, rejectOrConfirm } = updatedFormData[index];
+    const { _id, rejectOrConfirm, message } = updatedFormData[index];
 
     // Update the data in the MongoDB database
     axios.post('http://localhost:3001/updateFormData', {
       id: _id,
       rejectOrConfirm: rejectOrConfirm,
+      message: message,
     })
     .then(response => {
       console.log('Form data updated in MongoDB:', response.data);
@@ -60,94 +62,67 @@ function FormD({ showNotification }) {
     });
   };
 
-return (
+  const handleConfirm = (index) => {
+    const updatedFormData = [...formData];
+    updatedFormData[index].rejectOrConfirm = 'Confirmed';
+    updatedFormData[index].message = 'Request Confirmed';
+    setFormData(updatedFormData);
+    handleFormSubmit(index);
+  };
+
+  const handleReject = (index) => {
+    const updatedFormData = [...formData];
+    updatedFormData[index].rejectOrConfirm = 'Rejected';
+    updatedFormData[index].message = 'Request Rejected';
+    setFormData(updatedFormData);
+    handleFormSubmit(index);
+  };
+
+  // Reverse the order of formData to display LIFO
+  const reversedFormData = [...formData].reverse();
+
+  return (
     <div className="form-container">
-      <h1>Requests Form Data</h1>
-      <table className="data-table full-width">
-        <thead>
-          <tr>
-          <th>applicant
-                  name</th>
-                <th>appiicant
-                  Appoinment</th>
-                <th>vehicle
-                  Incharge</th>
-                <th>dateof
-                  Required</th>
-                <th>timeof
-                  Required</th>
-                <th>natureof
-                  Duty</th>
-                <th>address
-                  toGo</th>
-                <th>requirement</th>
-                <th>timetobe
-                  Spent</th>
-                <th>distance</th>
-                <th>dateof
-                  Arrival</th>
-                <th>timeof
-                  Arrival</th>
-                <th>numof
-                  Officers</th>
-                <th>numof
-                  Lectures</th>
-                <th>numof
-                  Instructors</th>
-                <th>numofcadet
-                  Officers</th>
-                <th>numofday
-                  Scholers</th>
-                <th>numof
-                  civilStaff</th>
-                <th>totalof
-                  Passengers</th>
-                <th>routeto
-                  Follow</th>
-                <th>dateof
-                  Apply</th>
-            <th>Reject or Confirm</th>
-            <th>Actions</th>
-          </tr>
-        </thead>
-        <tbody>
-          {formData.map((form, index) => (
-            <tr key={index}>
-              <td>{form.applicantname}</td>
-                <td>{form.appiicantAppoinment}</td>
-                <td>{form.vehicleIncharge}</td>
-                <td>{form.dateofRequired}</td>
-                <td>{form.timeofRequired}</td>
-                <td>{form.natureofDuty}</td>
-                <td>{form.addresstoGo}</td>
-                <td>{form.requirement}</td>
-                <td>{form.timetobeSpent}</td>
-                <td>{form.distance}</td>
-                <td>{form.dateofArrival}</td>
-                <td>{form.timeofArrival}</td>
-                <td>{form.numofOfficers}</td>
-                <td>{form.numofLectures}</td>
-                <td>{form.numofInstructors}</td>
-                <td>{form.numofcadetOfficers}</td>
-                <td>{form.numofdayScholers}</td>
-                <td>{form.numofcivilStaff}</td>
-                <td>{form.totalofPassengers}</td>
-                <td>{form.routetoFollow}</td>
-                <td>{form.dateofApply}</td>
-              <td bgcolor="#ffcc00">{form.rejectOrConfirm}</td>
-              <td>
-                <input
-                  type="text"
-                  onChange={e => handleRejectConfirmChange(index, e.target.value)}
-                />
-              </td>
-              <td>
-                <button className="action-button save-button" onClick={() => handleFormSubmit(index)}>Submit</button>
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+      <h1 className="request-head">Vehicle Request Forms</h1>
+      {/* Render each record in a separate box in LIFO order */}
+      {reversedFormData.map((form, index) => (
+        <div className="record-box" key={index}>
+          <p className="applicant-name">Applicant Name:{form.applicantname}</p>
+          <p className="requested-date">Requested date: {form.dateofApply}</p>
+          <p className="description">Description: {form.appiicantAppoinment}</p>
+        <div className="expand-arrow" onClick={() => toggleExpand(index)}>
+          <span>&#9660;</span> {/* Unicode arrow-down symbol */}
+       </div>
+
+       {/* Additional information */}
+        <div className={`additional-info ${form.isExpanded ? 'expanded' : ''}`}>
+            {/* Add more details here */}
+            <p>Additional Info: {form.additionalInfo}</p>
+        </div>
+
+
+        {/* Add more data fields as needed */}
+        <div className="reject-confirm-box">
+            <input
+              type="text"
+              onChange={e => handleRejectConfirmChange(index, e.target.value)}
+              value={form.rejectOrConfirm}
+            />
+            <button
+              className="action-button"
+              onClick={() => handleConfirm(index)}
+            >
+              Confirm
+            </button>
+            <button
+              className="action-button2"
+              onClick={() => handleReject(index)}
+            >
+              Reject
+            </button>
+        </div>
+    </div>
+    ))}
 
       {/* Render Notifications */}
       <div>
