b873d96ee5ec8f1a5e6f3b5c319aafc01779b8f4
MAVKT Muramudali <136581189+VilharaMuramudali@users.noreply.github.com>
2023-10-22 10:43:34 +0530

    Update index.js

--- Client/src/index.js
+++ Client/src/index.js
@@ -50,12 +50,11 @@ function index() {
     
     <BrowserRouter>
       <Routes>
-      <Route path="/" element={<First />} ></Route>
+        <Route path="/First" element={<First />} ></Route>
         <Route path="/register" element={<Signup />} ></Route>
         <Route path="/home" element={<Home />} ></Route>
         <Route path="/home1" element={<Home1 />} ></Route>
         <Route path="/home2" element={<Home2 />} ></Route>
-        <Route path="/First" element={<First />} ></Route>
         <Route path="/Ahome" element={<HomeA />} ></Route>
         <Route path="/FormD" element={<FormD />} ></Route>
         <Route path="/conformation" element={<Conformation />} ></Route>
